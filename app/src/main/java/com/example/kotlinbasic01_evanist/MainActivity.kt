29809aa3e5100a394ce40aeddc6bde7b0b9073b1
Evan_Park <seya3927@gmail.com>
2022-09-05 13:41:10 +1000

    에러 로그 찍어보기

--- app/src/main/java/com/example/kotlinbasic01_evanist/MainActivity.kt
+++ app/src/main/java/com/example/kotlinbasic01_evanist/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +19,7 @@ class MainActivity : AppCompatActivity() {
 //            이 {}안에 있는 코드만, 버튼이 눌렸을때 실행 됨.
 
             Log.d("메인화면로그", "클릭용 버튼 눌림")
+            Log.e("메인화면로그", "에러 관련 로그")
 
         }
 
